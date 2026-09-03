2d01a3b18905d12dcbe3bf143cd50c9d93ab1044
Semyon Kulakov <semen.kulakov@quadcode.com>
2026-09-03 22:52:19 +0300

    fix: deleted unnecessary columns

--- dbt/models/staging/stg_payment_engine.sql
+++ dbt/models/staging/stg_payment_engine.sql
@@ -16,16 +16,10 @@ select
     country,
     currency,
 
-    amount_local as amount_local_raw,
     cast(amount_local as decimal(20, 8)) as amount_local,
-    fx_rate_applied as fx_rate_applied_raw,
     cast(fx_rate_applied as decimal(18, 10)) as fx_rate_applied,
-    fx_date_applied as fx_date_applied_raw,
     cast(fx_date_applied as date) as fx_date_applied,
-    amount_usd as amount_usd_raw,
     cast(amount_usd as decimal(20, 8)) as amount_usd,
-    created_at_utc as created_at_utc_raw,
     cast(created_at_utc as timestamptz) as created_at_utc,
-    captured_at_utc as captured_at_utc_raw,
     cast(captured_at_utc as timestamptz) as captured_at_utc
 from {{ ref('payment_engine_log') }}
